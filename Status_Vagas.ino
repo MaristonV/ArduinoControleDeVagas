9074894311d44dff4267bd9f2f5448d451441a89
Máriston <vitoriosp1@gmail.com>
2019-12-10 12:35:23 -0300

    Update Status_Vagas.ino

--- Status_Vagas.ino
+++ Status_Vagas.ino
@@ -9,19 +9,23 @@
 #define WIFI_SSID "SANTANA"
 #define WIFI_PASSWORD "franca01"
 
-//Define trigger and echo digital pins
-const int trigPin = 4;
-const int echoPin = 3;
 
-// The amount of time the ultrassonic wave will be travelling for
-long duration = 0;
-// Define the distance variable
-double distance = 0;
-
-void setup()
-{
-
-    // Connect to Wi-Fi
+#define TRIGGER_PIN  5
+#define ECHO_PIN     4
+#define TRIGGER_PIN_1 12
+#define ECHO_PIN_1 13
+#define TRIGGER_PIN_2 0
+#define ECHO_PIN_2 2
+double accuracy    = 0.0;
+ int vagaDisponivel1 = 1; 
+ int vagaDisponivel = 1;
+ int vagasDisponiveis;
+ const int led = 16; 
+ const int led1 = 15;
+void setup() {
+  pinMode(led,OUTPUT);
+    pinMode(led1,OUTPUT);
+  // Connect to Wi-Fi
     Serial.print("Wi-Fi...");
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     Serial.print("Connecting...");
@@ -35,43 +39,89 @@ void setup()
     Serial.println(WiFi.localIP());
 
     Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-
-    // Ultrasonic sensor, set echo as Input and trigger as Output
-    pinMode(trigPin, OUTPUT);
-    pinMode(echoPin, INPUT);
-
-    Serial.begin(9600);
-}
-
-void loop()
-{
-
-    getDistance();
-    // Prints the distance value to the serial monitor
-    Serial.print("Distance: ");
-    Serial.println(distance);
+  Serial.begin (9600);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+   pinMode(TRIGGER_PIN_1, OUTPUT);
+  pinMode(ECHO_PIN_1 , INPUT);
+   pinMode(TRIGGER_PIN_2, OUTPUT);
+  pinMode(ECHO_PIN_2 , INPUT);
+  pinMode(BUILTIN_LED, OUTPUT);
   
-    delay(500);
-}
-
-void getDistance()
-{
-    // Clear trigPin
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-
-    // trigPin HIGH por 10ms
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-
-    //Reads echoPin, returns the travel time of the sound wave in ms
-    duration = pulseIn(echoPin, HIGH);
-
-    // Calculating the distance, in centimeters, using the formula described in the first section.
-    distance = duration * 0.034 / 2;
   
-    // Sends the distance value to Firebase
-    Firebase.setFloat("distance", distance);
+}
+
+void loop() {
+   
+  
+  sensor1();
+  sensor2();
+  sensor3();
+  vagasDisponiveis = vagaDisponivel + vagaDisponivel1;
+   Firebase.setInt("/sensores/vagasDisponiveis",vagasDisponiveis);
 
 }
+void sensor1(){
+  long duration, distance;
+   
+  digitalWrite(TRIGGER_PIN, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(TRIGGER_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = (duration/2) / 29.1;
+  if(distance < 15 ){
+    vagaDisponivel = 0;
+    digitalWrite(16, HIGH);
+    }else{
+       vagaDisponivel = 1;
+         digitalWrite(16, LOW);
+      }
+  Serial.print(distance);
+  Firebase.setInt("/sensores/vagaDisponivel",vagaDisponivel);
+  Serial.println(" Sensor1 cm");
+  delay(1000);
+  }
+
+  void sensor2(){
+  
+  long duration1, distance1;
+  digitalWrite(TRIGGER_PIN_1, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(TRIGGER_PIN_1, HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(TRIGGER_PIN_1, LOW);
+  duration1 = pulseIn(ECHO_PIN_1, HIGH);
+  distance1 = (duration1/2) / 29.1;  
+  if(distance1 < 15 ){
+    vagaDisponivel1 = 0;
+    digitalWrite(15, HIGH);
+    }else{
+      vagaDisponivel1 = 1;
+      digitalWrite(15, LOW);
+      }
+  Serial.print(distance1);
+  Firebase.setInt("/sensores/vagaDisponivel1",vagaDisponivel1);
+  Serial.println(" Sensor2 cm");
+  delay(1500);
+  }
+
+  void sensor3(){
+  long duration2, distance2;
+  digitalWrite(TRIGGER_PIN_2, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(TRIGGER_PIN_2, HIGH);  
+  delayMicroseconds(10); // Added this line
+  digitalWrite(TRIGGER_PIN_2, LOW);
+  duration2 = pulseIn(ECHO_PIN_2, HIGH);
+  distance2 = (duration2/2) / 29.1;
+  Serial.print(distance2);
+  Serial.println(" Sensor3 cm");
+  delay(10);
+  }
+
+ 
+
+
+
